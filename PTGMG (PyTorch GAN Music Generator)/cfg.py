b31aabadfebd546d325180b22db8ce6be9f9907c
Dmitry Fedorovich <coredov@gmail.com>
2023-08-10 21:31:55 +0300

    cfg.py correction

--- PTGMG (PyTorch GAN Music Generator)/cfg.py
+++ PTGMG (PyTorch GAN Music Generator)/cfg.py
@@ -1,6 +1,6 @@
 """
 ATTENTION! Update this variables if you want fine-tune or retrain model
-
+Меня попросили настроить, я в дуже не ебал что и написал это -Rlate0
 """
 
 def_audio_path = r"audios\atmo_phonk"
